2b9b05728d9119f8e2d211ef68491d0e3a434370
alanoud-alshammari28 <alanoud.alshammari7@gmail.com>
2026-07-26 23:23:34 +0300

    Update fetch_all.py

--- fetch_all.py
+++ fetch_all.py
@@ -2,10 +2,10 @@ import requests
 
 url = "https://masar-class-api.a-f-almatrafi.workers.dev/api/posts"
 while url:
- resp = requests.get(url, timeout=10)
+    resp = requests.get(url, timeout=10)
  if resp.status_code != 200:
      break
  data = resp.json()
  for post in data["data"]:
- print(post["id"], post["title"])
+     print(post["id"], post["title"])
  url = data["next"]
