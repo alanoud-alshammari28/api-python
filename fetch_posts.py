dee93f4782671f5a0147dd1f3319b7c4829c4f00
alanoud-alshammari28 <alanoud.alshammari7@gmail.com>
2026-07-26 23:09:59 +0300

    Update fetch_posts.py

--- fetch_posts.py
+++ fetch_posts.py
@@ -4,5 +4,5 @@ url = "https://masar-class-api.a-f-almatrafi.workers.dev/api/posts"
 resp = requests.get(url, params={"page": 1, "limit": 5}, timeout=10)
 print(resp.status_code)
 if resp.status_code == 200:
- for post in resp.json()["data"]:
- print(post["title"])
+for post in resp.json()["data"]:
+print(post["title"])
